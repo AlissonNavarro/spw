30b8287b91c275126ed85d01768ca06742fb00ad
ancouto <ancouto@SGN-DESEN-12.SGN-D01>
2015-01-29 16:28:18 -0300

    Ajustes temporarios

--- src/java/ClockManager/ClockManagerBean.java
+++ src/java/ClockManager/ClockManagerBean.java
@@ -6,7 +6,6 @@ package ClockManager;
 
 import Machines.Machine;
 import Machines.MachineBanco;
-import Usuario.UsuarioBean;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
--- src/java/Machines/MachineBanco.java
+++ src/java/Machines/MachineBanco.java
@@ -15,7 +15,6 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 //import java.util.logging.Logger;
 import javax.faces.model.SelectItem;
 
